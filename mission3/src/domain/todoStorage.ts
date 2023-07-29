4bc97d3f51b32d45f53264f5f146167ec40fc103
seeyoujeong <chan3358@naver.com>
2023-07-29 23:40:16 +0900

    feat: todoStorage 추가

---
+++ mission3/src/domain/todoStorage.ts
@@ -0,0 +1,26 @@
+import { getStorageItem, setStorageItem } from "../service";
+import { TodoState } from "../types";
+
+const TODO_STORAGE_KEY = "todo_storage";
+
+class StorageService<T> {
+  #storageKey;
+  #defaultValue;
+
+  constructor(storageKey: string, defaultValue: T) {
+    this.#storageKey = storageKey;
+    this.#defaultValue = defaultValue;
+  }
+
+  getData() {
+    return getStorageItem(this.#storageKey, this.#defaultValue);
+  }
+
+  setData(data: TodoState) {
+    return setStorageItem(this.#storageKey, data);
+  }
+}
+
+const todoStorage = new StorageService<TodoState[]>(TODO_STORAGE_KEY, []);
+
+export default todoStorage;
