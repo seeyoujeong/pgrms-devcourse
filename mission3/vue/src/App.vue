d4093974a4b241869f4dff87a509ccadea9ce776
seeyoujeong <chan3358@naver.com>
2023-08-01 00:13:40 +0900

    feat: mission3에 vue 프로젝트 시작 및 Header 컴포넌트 추가

---
+++ mission3/vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <Header title="Todo List" />
+</template>
+
+<style scoped>
+  #app {
+    margin-left: 20px;
+  }
+</style>
---
+++ mission3/vue/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<style scoped>
+</style>
