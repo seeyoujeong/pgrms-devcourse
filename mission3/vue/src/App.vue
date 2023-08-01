a37aada1effbf4cd666d02fccc31d8606d28597b
seeyoujeong <chan3358@naver.com>
2023-08-02 00:33:07 +0900

    feat: TodoCount 컨포넌트 template 추가 및 computed에 countTodos() 추가

--- mission3/vue/src/App.vue
+++ mission3/vue/src/App.vue
@@ -2,18 +2,21 @@
   <Header title="Todo List" />
   <TodoFrom />
   <TodoList :todos="todos" />
+  <TodoCount :count="countTodos" />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import TodoFrom from "./components/TodoForm.vue";
 import TodoList from "./components/TodoList.vue";
+import TodoCount from "./components/TodoCount.vue";
 
 export default {
   components: {
     Header,
     TodoFrom,
-    TodoList
+    TodoList,
+    TodoCount
   },
   data() {
     return {
@@ -24,6 +27,14 @@ export default {
         }
       ]
     };
+  },
+  computed: {
+    countTodos() {
+      return {
+        completedCount: this.todos.filter(({_, isCompleted}) => isCompleted).length,
+        totalCount: this.todos.length
+      }
+    }
   }
 }
 </script>
---
+++ mission3/vue/src/components/TodoCount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Completed: {{ count.completedCount }} / Total: {{ count.totalCount }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Object,
+      default: () => ({
+        completedCount: 0,
+        totalCount: 0,
+      })
+    }
+  }
+}
+</script>
