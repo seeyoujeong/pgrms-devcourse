c0028816b4caff95b06b0b4b7869f39b808e26a4
seeyoujeong <chan3358@naver.com>
2023-08-01 20:48:06 +0900

    fix: progress bar에 NaN이 전달되는 오류 해결

--- mission3/vanilla-ts/src/components/TodoCount.ts
+++ mission3/vanilla-ts/src/components/TodoCount.ts
@@ -22,13 +22,12 @@ export default class TodoCount extends Component<TodoCountProps> {
   protected render() {
     const { completedCount, totalCount } =
       this.state ?? this.props.initialState;
+    const percent = (completedCount / totalCount) * 100 || 0;
 
     this.$target.innerHTML = `
       <div>Completed: ${completedCount} / Total: ${totalCount}</div>
       <div class="todo-count__progress-bar">
-        <div class="todo-count__progress-fill" style="--percent: ${
-          (completedCount / totalCount) * 100
-        }"></div>
+        <div class="todo-count__progress-fill" style="--percent: ${percent}"></div>
       </div>
     `;
   }
